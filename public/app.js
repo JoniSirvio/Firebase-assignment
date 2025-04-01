0b868a7752cfdb09c666e188b5239797e5ab53a1
Joni Sirviö <t3sijo05@students.oamk.fi>
2025-04-01 10:50:22 +0300

    Hide API key

--- public/app.js
+++ public/app.js
@@ -1,15 +1,6 @@
+import firebaseConfig from '../firebaseconfig.js';
 import { initializeApp } from "https://www.gstatic.com/firebasejs/11.5.0/firebase-app.js";
 import { getFirestore, collection, addDoc, getDocs, onSnapshot } from "https://www.gstatic.com/firebasejs/11.5.0/firebase-firestore.js";
-
-const firebaseConfig = {
-    apiKey: "AIzaSyDdzpB9RIjmqlbY7JCiyU7jVt9D8Gi47QE",
-    authDomain: "fir-assignment-a5bae.firebaseapp.com",
-    projectId: "fir-assignment-a5bae",
-    storageBucket: "fir-assignment-a5bae.appspot.com",
-    messagingSenderId: "68096552344",
-    appId: "1:68096552344:web:df7f8f1e83170ad7448fed",
-    measurementId: "G-4506J7FCGG"
-};
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
